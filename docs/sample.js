4ed5f18e21738e812a1138c6a569eae997b6467d
john <john>
2017-02-06 15:49:32 +0000

    Ready for release.

--- docs/sample.js
+++ docs/sample.js
@@ -1,4 +1,4 @@
-const gem = require('./lib/index.js')
+const gem = require('../lib/index.js')
 // create a new geometry document
 const geometry = gem.geometry()
 // add pa_define, include and comment statements
@@ -6,10 +6,11 @@ geometry.paDefine(101,101,1,'planar','None','electrostatic', 1)
   .include('some-file')
   .comment('some comment')
 // add an electrode
-const e1 = geometry.append(gem.electrode(1))
+const e1 = geometry.append(gem.locate(20,20,20)).append(gem.electrode(1))
 // add a nested electrode
 e1.append(gem.electrode(2))
 // render the document
-e1.append(gem.element('fill'))
-  .append(gem.element('sphere',0,0,0,45,25,25))
+e1.append('fill')
+  .append('within')
+  .append('sphere',0,0,0,45,25,25)
 console.log(geometry.render())
